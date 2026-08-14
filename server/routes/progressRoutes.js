a01c83e01e57f6d6f9cb834284c76814f1e2169c
Mandira Nirankari <mandiranirankari@gmail.com>
2026-08-15 00:07:24 +0530

    Fix progress model import

--- server/routes/progressRoutes.js
+++ server/routes/progressRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const Progress = require('../models/Progress')
+const Progress = require('../models/progress')
 const protect = require('../middleware/authMiddleware')
 
 const router = express.Router()
